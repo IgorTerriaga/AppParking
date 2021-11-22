43e63e0f5ae60f4bd1158676e2b2bfee7cfc702d
IgorTerriaga <igsantos1996@gmail.com>
2021-11-22 15:26:10 -0300

    Segunda Tela ok

--- app/src/main/java/com/example/appparking/MainActivity.java
+++ app/src/main/java/com/example/appparking/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         buttonNew.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent =new Intent(getApplicationContext(), RegisterActivity.class);
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
                 startActivity(intent);
             }
         });

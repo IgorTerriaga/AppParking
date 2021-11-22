964a9587386189eba39730310a983c24e4236af5
IgorTerriaga <igsantos1996@gmail.com>
2021-11-22 16:19:13 -0300

    Terceira Tela OK

--- app/src/main/java/com/example/appparking/MainActivity.java
+++ app/src/main/java/com/example/appparking/MainActivity.java
@@ -6,16 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button buttonNew;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        buttonNew = findViewById(R.id.buttonNew);
+        buttonNew = findViewById(R.id.buttonNext);
         buttonNew.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/example/appparking/RegisterActivity.java
+++ app/src/main/java/com/example/appparking/RegisterActivity.java
@@ -2,13 +2,26 @@ package com.example.appparking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private Button next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        next = findViewById(R.id.buttonNext);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Register2Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

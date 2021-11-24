0951c29a5c23fc2e2170c06d6e20c56b6ba9c74a
IgorTerriaga <igsantos1996@gmail.com>
2021-11-23 21:37:06 -0300

    segunda tela ok

--- app/src/main/java/com/example/appparking/MainActivity.java
+++ app/src/main/java/com/example/appparking/MainActivity.java
@@ -6,9 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button buttonNew;
+    private TextView hasAcc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +18,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         buttonNew = findViewById(R.id.buttonNext);
+        hasAcc = findViewById(R.id.textHasAccount);
+
         buttonNew.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        hasAcc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent2 = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent2);
             }
         });
     }
---
+++ app/src/main/java/com/example/appparking/utils/Requests.java
@@ -0,0 +1,6 @@
+package com.example.appparking.utils;
+
+public class Requests {
+    private final String baseURL = "localhost:5000/";
+    
+}

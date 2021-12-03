a995411e3cd1693dbf026edab42ef772aeea95a6
Igor Terriaga <igsantos1996@gmail.com>
2021-12-02 22:51:21 -0300

    removendo o shared preferences

--- app/src/main/java/com/example/appparking/LoginActivity.java
+++ app/src/main/java/com/example/appparking/LoginActivity.java
@@ -30,7 +30,7 @@ public class LoginActivity extends AppCompatActivity {
     private TextInputEditText senha;
     private Button login;
     private Retrofit retrofit;
-    private CheckBox checkBox;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +38,6 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
 
-        checkBox = findViewById(R.id.checkBoxLembrar);
         email = findViewById(R.id.TextInputEditEmail);
         senha = findViewById(R.id.TextInputEditSenha);
 
@@ -56,32 +55,9 @@ public class LoginActivity extends AppCompatActivity {
                 logar();
             }
         });
-
-        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (buttonView.isChecked()) {
-                    SharedPreferences preferences = getSharedPreferences("checkbox", MODE_PRIVATE);
-                    SharedPreferences.Editor editor = preferences.edit();
-                    editor.putString("checkbox", "true");
-                    editor.apply();
-                    Toast.makeText(LoginActivity.this, "Checked", Toast.LENGTH_LONG).show();
-
-                } else if (!buttonView.isChecked()) {
-                    SharedPreferences preferences = getSharedPreferences("checkbox", MODE_PRIVATE);
-                    SharedPreferences.Editor editor = preferences.edit();
-                    editor.putString("checkbox", "false");
-                    editor.apply();
-                    Toast.makeText(LoginActivity.this, "UnChecked", Toast.LENGTH_LONG).show();
-                }
-            }
-        });
     }
 
     private void logar() {
-
-        SharedPreferences preferences = getSharedPreferences("checkbox", MODE_PRIVATE);
-        String checkbox = preferences.getString("checkbox", "");
 
         Login login = new Login(email.getText().toString().trim(), senha.getText().toString().trim());
 
@@ -94,15 +70,12 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<Login> call, Response<Login> response) {
                 if (response.isSuccessful()) {
-                    if (checkbox.equals("true")) {
-                        Intent intent = new Intent(getApplicationContext(), WelcomeActivity.class);
-                        startActivity(intent);
-
-                    } else if (checkbox.equals("false")) {
-                        Toast.makeText(getApplicationContext(), "Por favor, faça o login.", Toast.LENGTH_SHORT).show();
-                    }
-                    System.out.println(response.code());
                     Toast.makeText(getApplicationContext(), "Login efetuado com sucesso", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getApplicationContext(), WelcomeActivity.class);
+                    intent.putExtra("email", email.getText().toString().trim());
+                    startActivity(intent);
+
+                    System.out.println(response.code());
                 } else if (response.code() == 500) {
                     System.out.println("Erro: " + response.message() + " " + " " + response.toString());
                     Toast.makeText(getApplicationContext(), "Erro no Server", Toast.LENGTH_SHORT).show();
--- app/src/main/java/com/example/appparking/WelcomeActivity.java
+++ app/src/main/java/com/example/appparking/WelcomeActivity.java
@@ -2,27 +2,33 @@ package com.example.appparking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class WelcomeActivity extends AppCompatActivity {
     private Button logout;
+    private TextView text;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        text = findViewById(R.id.textEmail);
+
+        Bundle bundle = getIntent().getExtras();
+        String email = bundle.getString("email");
+        text.setText(email);
 
         logout = findViewById(R.id.buttonLogout);
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SharedPreferences preferences = getSharedPreferences("checkbox", MODE_PRIVATE);
-                SharedPreferences.Editor editor = preferences.edit();
-                editor.putString("checkbox", "false");
-                editor.apply();
+
                 finish();
             }
         });

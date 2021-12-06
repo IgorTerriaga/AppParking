e4bb97cb5b8ed16e8880eae9739862c7e7761201
IgorTerriaga <igsantos1996@gmail.com>
2021-12-06 11:58:36 -0300

    Utilizando o SmartTabLayout

--- app/src/main/java/com/example/appparking/activities/MainActivity.java
+++ app/src/main/java/com/example/appparking/activities/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
         buttonNew.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                Intent intent = new Intent(getApplicationContext(), WelcomeActivity.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/appparking/activities/WelcomeActivity.java
+++ app/src/main/java/com/example/appparking/activities/WelcomeActivity.java
@@ -1,32 +1,50 @@
 package com.example.appparking.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentPagerAdapter;
-
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.appparking.Fragments.LojasFragment;
+import com.example.appparking.Fragments.MaisOcupadasFragment;
+import com.example.appparking.Fragments.MeuCarroFragment;
 import com.example.appparking.R;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
 
 public class WelcomeActivity extends AppCompatActivity {
     private Button logout;
+    private SmartTabLayout smartTabLayout;
+    private ViewPager viewPager;
     private TextView text;
+    private TextView textLojas, textCarros, textVagasMais;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_welcome);
-        text = findViewById(R.id.textEmail);
+        smartTabLayout = findViewById(R.id.viewPagerTab);
+        viewPager = findViewById(R.id.viewPager);
+        getSupportActionBar().setElevation(0);
+        logout = findViewById(R.id.buttonLogout);
 
-        Bundle bundle = getIntent().getExtras();
-        String email = bundle.getString("email");
-        text.setText(email);
-
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(),
+                FragmentPagerItems.with(this)
+                        .add("Lojas", LojasFragment.class)
+                        .add("Vaga mais Ocupadas", MaisOcupadasFragment.class)
+                        .add("Meu Veiculo", MeuCarroFragment.class)
+                        .create()
+        );
+        viewPager.setAdapter(adapter);
+        smartTabLayout.setViewPager(viewPager);
         logout = findViewById(R.id.buttonLogout);
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
